ecc7f5c8b68daa578f05dc802f22a8d0ec9c41c0
Céline Bréchet <celinebrechet@hotmail.fr>
2021-02-17 08:12:54 +0100

    Céline

---
+++ Groupe 2/js/chap_02.js
@@ -0,0 +1,6 @@
+//Exo2 Céline
+$("#titreCV").text("Aventurière et archéologue");
+$("#divAge").text("138 ans");
+$(".titreSection").css("background-color", "yellow");
+$(".dateCV").css("font-weight", "bold");
+//
